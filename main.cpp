36aed92360fa2d68b3637371fa686bcb9f03f5d3
Nicholas <161537252+YALOKGARua@users.noreply.github.com>
2024-08-04 17:27:19 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,18 +4,14 @@
 #include "integrity.h"
 
 int main() {
-    // Create a Logger instance
     Logger logger("application.log");
     logger.log("Application started");
 
-    // Create a Settings instance
     Settings settings("config.txt");
 
-    // Create a Menu instance
     Menu menu;
     menu.handleInput();
 
-    // Log application end
     logger.log("Application ended");
     return 0;
 }
